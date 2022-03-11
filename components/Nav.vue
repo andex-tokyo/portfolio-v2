93360d2b8b40e787f8c01487a2c4164bb029c2ad
Yuki Tsuchida <d@gahaku.tech>
2022-03-11 11:20:07 +0900

    fix nav link

--- components/Nav.vue
+++ components/Nav.vue
@@ -3,9 +3,9 @@
     <nav>
       <div class="inner">
         <ul>
-          <li><a href="#works">-Works-</a></li>
-          <li><a href="#about">-About-</a></li>
-          <li><a href="#contact">-Contact-</a></li>
+          <li><a href="/#works">-Works-</a></li>
+          <li><a href="/#about">-About-</a></li>
+          <li><a href="/#contact">-Contact-</a></li>
         </ul>
       </div>
     </nav>
